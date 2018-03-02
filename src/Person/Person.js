45be75863a976ecbfd885900d570d9daaf48d9ca
Vamsi Devalla <vammichanti40312@gmail.com>
2018-03-02 10:58:12 -0500

    created Person.css file to write the style for Person component, imported it into Person.js file, given className of Person.js to Person componet, updated Person.css with all the stying required.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     return(
-        <div>
+        <div className="Person">
          <h1 onClick={props.click}>This is {props.name}</h1>
          <input onChange={props.changed} value={props.name}/>
          <p>{props.children}</p>
         </div>
     )
 } 
-
+ 
 export default person
